ad7c655bee1d5afb856624e68c916f8a92db273f
kushkp <ankushp89@gmail.com>
2015-08-12 16:30:03 -0700

    implement file upload

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
-  validates :album_id, :title, :url, :owner_id, presence: true
+  validates :title, :url, :owner_id, presence: true
 
   belongs_to :album
   has_one :owner, through: :album, source: :owner
